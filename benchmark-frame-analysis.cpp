cdda7d0703c36f2de54c0fa939c8743e48cfec6d
Adam Casey <adamjcasey@gmail.com>
2017-01-21 04:39:47 +0000

    Working with raspicam library, framerate and zoom good

--- benchmark-frame-analysis.cpp
+++ benchmark-frame-analysis.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <chrono>
 #include <cmath>
-
+#include <raspicam/raspicam_cv.h>
 
 class BenchmarkCase
 {
@@ -31,30 +31,14 @@ class BenchmarkCase
 
 BenchmarkCase testcases[] = 
 {
-    BenchmarkCase(640.0f, 480.0f, 1.0f),
-    BenchmarkCase(640.0f, 480.0f, 2.0f),
-    BenchmarkCase(640.0f, 480.0f, 3.0f),
-    BenchmarkCase(640.0f, 480.0f, 4.0f),
-    BenchmarkCase(640.0f, 480.0f, 5.0f),
-    BenchmarkCase(640.0f, 480.0f, 6.0f),
-    BenchmarkCase(640.0f, 480.0f, 7.0f),
-    BenchmarkCase(640.0f, 480.0f, 8.0f),
-    BenchmarkCase(1280.0f, 720.0f, 1.0f),
-    BenchmarkCase(1280.0f, 720.0f, 2.0f),
-    BenchmarkCase(1280.0f, 720.0f, 3.0f),
-    BenchmarkCase(1280.0f, 720.0f, 4.0f),
-    BenchmarkCase(1280.0f, 720.0f, 5.0f),
-    BenchmarkCase(1280.0f, 720.0f, 6.0f),
-    BenchmarkCase(1280.0f, 720.0f, 7.0f),
-    BenchmarkCase(1280.0f, 720.0f, 8.0f)
-    // BenchmarkCase(1920.0f, 1080.0f, 1.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 2.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 3.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 4.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 5.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 6.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 7.0f),
-    // BenchmarkCase(1920.0f, 1080.0f, 8.0f)
+    BenchmarkCase(1280.0f, 960.0f, 1.0f),
+    BenchmarkCase(1280.0f, 960.0f, 2.0f),
+    BenchmarkCase(1280.0f, 960.0f, 3.0f),
+    BenchmarkCase(1280.0f, 960.0f, 4.0f),
+    BenchmarkCase(1280.0f, 960.0f, 5.0f),
+    BenchmarkCase(1280.0f, 960.0f, 6.0f),
+    BenchmarkCase(1280.0f, 960.0f, 7.0f),
+    BenchmarkCase(1280.0f, 960.0f, 8.0f)
 };
 
 int num_test_cases = sizeof(testcases)/sizeof(BenchmarkCase);
@@ -62,90 +46,89 @@ int num_test_cases = sizeof(testcases)/sizeof(BenchmarkCase);
 int main()
 {
     std::cout << "Built with OpenCV " << CV_VERSION << std::endl;
+    raspicam::RaspiCam_Cv camera;
     cv::Mat image;
-    cv::VideoCapture capture;
-    capture.open(0);
-    if (capture.isOpened())
-    {
-        std::cout << "Capture is opened" << std::endl;
+	camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+	if (!camera.open()) 
+	{
+		std::cout << "Error opening the camera" << std::endl;
+		return -1;
+	}
+	std::cout << "Camera is opened" << std::endl;
+    cvNamedWindow("Name", CV_WINDOW_NORMAL);
+    cvSetWindowProperty("Name", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+	// Cycle through the test cases
+	for (int i=0; i<num_test_cases; i++)
+	{
+		// Loop through the captures            
+		for (int j=0; j<BenchmarkCase::NUM_FRAMES_PER_CASE; j++)
+		{
+			testcases[i].start_time[j] = std::chrono::high_resolution_clock::now();
 
-        // Cycle through the test cases
-        for (int i=0; i<num_test_cases; i++)
-        {
-            // Set the height and the width
-            capture.set(cv::CAP_PROP_FRAME_WIDTH, testcases[i].width);
-            capture.set(cv::CAP_PROP_FRAME_HEIGHT, testcases[i].height);
+			//-- CAPTURE --------------------------------------------------------------------------
+			camera.grab();
+			camera.retrieve(image);
+			
+			testcases[i].capture_time[j] = std::chrono::high_resolution_clock::now();
 
+			//-- ZOOM -----------------------------------------------------------------------------
+			// Calculate the region of interest of the zoom
+			int x = floor((((testcases[i].width / testcases[i].zoom) * (testcases[i].zoom / 2.0)) - ((testcases[i].width / testcases[i].zoom) / 2.0)));
+			int y = floor((((testcases[i].height / testcases[i].zoom) * (testcases[i].zoom / 2.0))- ((testcases[i].height / testcases[i].zoom) / 2.0)));
+			int width = floor((testcases[i].width / testcases[i].zoom));
+			int height = floor((testcases[i].height / testcases[i].zoom));
 
-            // Loop through the captures            
-            for (int j=0; j<BenchmarkCase::NUM_FRAMES_PER_CASE; j++)
-            {
-                testcases[i].start_time[j] = std::chrono::high_resolution_clock::now();
+			// Perform the zoom
+			cv::Rect new_size(x, y, width, height);
+			cv::Mat tmp = image(new_size);
+			cv::resize(tmp, tmp, cv::Size(testcases[i].width, testcases[i].height), 0, 0, CV_INTER_LINEAR);
 
-                //-- CAPTURE --------------------------------------------------------------------------
-                capture >> image;
-                testcases[i].capture_time[j] = std::chrono::high_resolution_clock::now();
+			testcases[i].zoom_time[j] = std::chrono::high_resolution_clock::now();
 
-                //-- ZOOM -----------------------------------------------------------------------------
-                // Calculate the region of interest of the zoom
-                int x = floor((((testcases[i].width / testcases[i].zoom) * (testcases[i].zoom / 2.0)) - ((testcases[i].width / testcases[i].zoom) / 2.0)));
-                int y = floor((((testcases[i].height / testcases[i].zoom) * (testcases[i].zoom / 2.0))- ((testcases[i].height / testcases[i].zoom) / 2.0)));
-                int width = floor((testcases[i].width / testcases[i].zoom));
-                int height = floor((testcases[i].height / testcases[i].zoom));
+			//-- CONTRAST --------------------------------------------------------------------------
+			//float alpha = 2.0f;
+			//int beta = 0;
+			//cv::Mat tmp2 = cv::Mat::zeros(tmp.size(), tmp.type());
+			//tmp.convertTo(tmp2, -1, alpha, beta);
 
-                // Perform the zoom
-                cv::Rect new_size(x, y, width, height);
-                cv::Mat tmp = image(new_size);
-                cv::resize(tmp, tmp, cv::Size(testcases[i].width, testcases[i].height), 0, 0, CV_INTER_LINEAR);
+			testcases[i].contrast_time[j] = std::chrono::high_resolution_clock::now();
 
-                testcases[i].zoom_time[j] = std::chrono::high_resolution_clock::now();
+			//-- COLOR BALANCE ---------------------------------------------------------------------
+			//float percent = 1.0;
+			//float half_percent = percent / 200.0f;
 
-                //-- CONTRAST --------------------------------------------------------------------------
-                float alpha = 2.0f;
-                int beta = 0;
-                cv::Mat tmp2 = cv::Mat::zeros(tmp.size(), tmp.type());
-                tmp.convertTo(tmp2, -1, alpha, beta);
+			//std::vector<cv::Mat> tmpsplit; 
+			//split(tmp2, tmpsplit);
+			//for (int i=0; i<3; i++) 
+			//{
+				//// Find the low and high precentile values (based on the input percentile)
+				//cv::Mat flat; 
+				//tmpsplit[i].reshape(1, 1).copyTo(flat);
+				//cv::sort(flat, flat, CV_SORT_EVERY_ROW + CV_SORT_ASCENDING);
+				//int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
+				//int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));
+	
+				////saturate below the low percentile and above the high percentile
+				//tmpsplit[i].setTo(lowval,tmpsplit[i] < lowval);
+				//tmpsplit[i].setTo(highval,tmpsplit[i] > highval);
+				
+				////scale the channel
+				//cv::normalize(tmpsplit[i], tmpsplit[i], 0, 255, cv::NORM_MINMAX);
+			//}
+			//merge(tmpsplit, tmp2);
 
-                testcases[i].contrast_time[j] = std::chrono::high_resolution_clock::now();
+			testcases[i].cb_time[j] = std::chrono::high_resolution_clock::now();
 
-                //-- COLOR BALANCE ---------------------------------------------------------------------
-                float percent = 1.0;
-                float half_percent = percent / 200.0f;
-
-                std::vector<cv::Mat> tmpsplit; 
-                split(tmp2, tmpsplit);
-                for (int i=0; i<3; i++) 
-                {
-                    // Find the low and high precentile values (based on the input percentile)
-                    cv::Mat flat; 
-                    tmpsplit[i].reshape(1, 1).copyTo(flat);
-                    cv::sort(flat, flat, CV_SORT_EVERY_ROW + CV_SORT_ASCENDING);
-                    int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
-                    int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));
-        
-                    //saturate below the low percentile and above the high percentile
-                    tmpsplit[i].setTo(lowval,tmpsplit[i] < lowval);
-                    tmpsplit[i].setTo(highval,tmpsplit[i] > highval);
-                    
-                    //scale the channel
-                    cv::normalize(tmpsplit[i], tmpsplit[i], 0, 255, cv::NORM_MINMAX);
-                }
-                merge(tmpsplit, tmp2);
-
-                testcases[i].cb_time[j] = std::chrono::high_resolution_clock::now();
-
-                //-- INVERT -----------------------------------------------------------------------------
-
-
-                //-- DISPLAY -----------------------------------------------------------------------------
-                cv::imshow("Sample", tmp2);
-                testcases[i].display_time[j] = std::chrono::high_resolution_clock::now();
+			//-- INVERT -----------------------------------------------------------------------------
 
 
+			//-- DISPLAY -----------------------------------------------------------------------------
+			cv::imshow("Name", tmp);
+			testcases[i].display_time[j] = std::chrono::high_resolution_clock::now();
 
-                cv::waitKey(1);
 
-            }
+
+			cv::waitKey(1);
         }
     }
 
